4173dbdb368ddfa990cd1edfc0857a42f51ff3c0
mizutsuki1980 <mizutuki1980@gmail.com>
2025-05-27 09:39:29 +0900

    test

--- app/src/main/java/com/example/jajajamaru/MainActivity.kt
+++ app/src/main/java/com/example/jajajamaru/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.jajajamaru
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
-    val handler = android.os.Handler()
+
+    val handler = Handler()
     lateinit var custom : MyCustomView
 
     override fun onCreate(savedInstanceState: Bundle?) {
