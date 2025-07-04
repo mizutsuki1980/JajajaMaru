4d84e2cea6855491f66b3180dca188b17b2ed991
mizutsuki1980 <mizutuki1980@gmail.com>
2025-07-04 13:55:31 +0900

    とりあえず保存

--- app/src/main/java/com/example/jajajamaru/Controller.kt
+++ app/src/main/java/com/example/jajajamaru/Controller.kt
@@ -8,9 +8,11 @@ import android.graphics.Rect
 
 class Controller {
     var houkou = "nashi"
-    var isFirstJump = false
+    var isJump = false
+    var jumpFrame = 0
+    var jumpTakasa = 0
 
-    fun draw(canvas: Canvas,clickX:Int,clickY:Int,clickState:String) {
+    fun draw(canvas: Canvas,clickX:Int,clickY:Int,clickState:String){
         houkou = "nashi"
         val hidariButtonIro = Paint()
         hidariButtonIro.style = Paint.Style.FILL
@@ -31,15 +33,21 @@ class Controller {
         val buttonPushIro = Paint()
         buttonPushIro.style = Paint.Style.FILL
         buttonPushIro.color = argb(100, 0, 0, 150)
+        //plphaを下げると暗くなる
 
-        val hyoujiIro = Paint()
+
+        val hyoujiIro =  Paint()
         hyoujiIro.style = Paint.Style.FILL
         hyoujiIro.color = Color.BLUE
         hyoujiIro.textSize = 100.toFloat()
 
-        //左ボタン処理
-        if (clickX > 50 && clickX < 150) {
-            if (clickY > 920 && clickY < 1070) {
+        //xが50～150の間で
+        //ｙが920～1070の間で
+        //onTouchEventが押してる状態だったら
+        //色を変える
+
+        if(clickX > 50 && clickX <150){
+            if(clickY > 920 && clickY <1070) {
                 if (clickState == "ACTION_DOWN" || clickState == "ACTION_MOVE") {
                     houkou = "hidari"
                     hidariButtonIro.color = argb(100, 100, 100, 200)
@@ -47,9 +55,9 @@ class Controller {
             }
         }
 
-        //右ボタン処理
-        if (clickX > (30 + 170 + 170 + 170) && clickX < (30 + 170 + 170 + 170 + 150)) {
-            if (clickY > 920 && clickY < 1070) {
+
+        if(clickX > (30+170+170+170) && clickX <(30+170+170+170+150)){
+            if(clickY > 920 && clickY < 1070) {
                 if (clickState == "ACTION_DOWN" || clickState == "ACTION_MOVE") {
                     houkou = "migi"
                     migiButtonIro.color = argb(100, 100, 100, 200)
@@ -57,31 +65,28 @@ class Controller {
             }
         }
 
-        //中央ボタン処理
-        if (clickX > (170) && clickX < (30 + 170 + 320)) {
-            if (clickY > 920 && clickY < 1070) {
+        if(clickX > (170) && clickX <(30+170+320)){
+            if(clickY > 920 && clickY < 1070) {
                 if (clickState == "ACTION_DOWN" || clickState == "ACTION_MOVE") {
-                    isFirstJump = true
+                    isJump = true
                     jumpButtonIro.color = argb(100, 100, 100, 200)
-                }
-                if (clickState == "ACTION_UP") {
-                    isFirstJump = false
                 }
             }
         }
-            //左ボタン
-            canvas.drawRect(shikakuRectButton(30.toInt(), 920.toInt(), 150), hidariButtonIro)
-            canvas.drawText("←", (50).toFloat(), (1035).toFloat(), hyoujiIro)
 
-            //中央ボタン
-            canvas.drawRect(shikakuYokonagaRectButton( 30 + 170.toInt(), 920.toInt(), 320, 150), jumpButtonIro)
-            canvas.drawText("Jump!", (50 + 170).toFloat(), (1035).toFloat(), hyoujiIro)
+        canvas.drawRect(shikakuRectButton(30.toInt() ,920.toInt(),150), hidariButtonIro)   //
+        canvas.drawText("←",(50).toFloat(),(1035).toFloat(),hyoujiIro)
+        //canvas.drawRect(shikakuRectButton(30+170.toInt() ,920.toInt(),150), buttonIro)   //
 
-            //右ボタン
-            canvas.drawRect(shikakuRectButton(30 + 170 + 170 + 170.toInt(), 920.toInt(), 150), migiButtonIro)
-            canvas.drawText("→", (570).toFloat(), (1035).toFloat(), hyoujiIro)
-        }
+        canvas.drawRect(shikakuYokonagaRectButton(30+170.toInt() ,920.toInt(),320,150
+        ), jumpButtonIro)
+        canvas.drawText("Jump!",(50+170).toFloat(),(1035).toFloat(),hyoujiIro)
+
+
+        canvas.drawRect(shikakuRectButton(30+170+170+170.toInt() ,920.toInt(),150), migiButtonIro)   //
+        canvas.drawText("→",(570).toFloat(),(1035).toFloat(),hyoujiIro)
     }
+
     fun shikakuYokonagaRectButton(xxx:Int,yyy:Int,yokoooookisa:Int,tateooookisa:Int): Rect {
         val left = xxx
         val right = xxx  + yokoooookisa
@@ -99,3 +104,15 @@ class Controller {
         val m = Rect(left, top, right,bottom)
         return m
     }
+
+    //中心をｘｙとしてookisaの四角を描画する
+    fun shikakuRectXYSub(xxx:Int,yyy:Int,ooookisa:Int): Rect {
+        val left = xxx  - ooookisa / 2
+        val right = xxx  + ooookisa / 2
+        val top = yyy  - ooookisa / 2
+        val bottom = yyy + ooookisa / 2
+        val m = Rect(left, top, right,bottom)
+        return m
+    }
+
+}
