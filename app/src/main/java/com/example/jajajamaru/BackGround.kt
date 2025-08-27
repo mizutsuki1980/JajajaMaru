3307ffbb2ebec9eb1b7732cc7c90b3f3c51806f8
mizutsuki1980 <mizutuki1980@gmail.com>
2025-08-27 13:27:17 +0900

    へんこう

--- app/src/main/java/com/example/jajajamaru/BackGround.kt
+++ app/src/main/java/com/example/jajajamaru/BackGround.kt
@@ -20,6 +20,13 @@ var ookisa = 500
         //なんかできないっぽい
         //canvas.drawBitmap(bitmap, 50.0F+(background.x.toFloat()), 200.0F+(background.y.toFloat()), null)
         //canvas.drawCircle(x.toFloat(),(y).toFloat(),(ookisa).toFloat(),iro) //自機の描画
+        var itemList = mutableListOf<Int>(1,2,3,4,5)
+
+        for (i in itemList) {
+            iro.color = argb(15*i, 155, 155, 150)
+            canvas.drawRect(x+100*i.toFloat(),y.toFloat(),(x+200*i).toFloat(),(y+500).toFloat(),iro)
+        }
+
         canvas.drawRect(x.toFloat(),y.toFloat(),(x+200).toFloat(),(y+500).toFloat(),iro)
 
     }
