038d853dde0b0700568177b104fb517e2445719e
mizutsuki1980 <mizutuki1980@gmail.com>
2025-09-05 14:20:14 +0900

    コメント修正

--- app/src/main/java/com/example/jajajamaru/MyCustomView.kt
+++ app/src/main/java/com/example/jajajamaru/MyCustomView.kt
@@ -9,6 +9,7 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 
+
 class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     var frame = 0
     var clickState = "nashi"
@@ -41,15 +42,16 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
 
 
 
+
+
     //加速度
-    var v = 0f
-    var zenkaihoukou = "nashi"
-    fun kasokudo(houkou:String):Float {
+    var vYoko = 0f
+    fun kasokudoYoko(houkou:String):Float {
         when (houkou) {
             "migi" -> { return 1.0f }
             "hidari" -> { return -1.0f }
             "nashi" -> {
-                if(v>0){
+                if( vYoko > 0 ){
                     return-2.5f
                 }else{
                     return 2.5f}
@@ -57,23 +59,44 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
             else -> return 0f
         }
     }
-
-    fun tsugiNoIchi(houkou:String):Int{
-        v = v + kasokudo(houkou)
-        return v.toInt()
+    //加速度で距離を求めて自機のｘに足す
+    fun jikiNoIchiYoko(){
+        vYoko = vYoko + kasokudoYoko(controller.houkou)
+        jiki.x += vYoko.toInt()
     }
 
 
-    fun jikiNoIchi(){
-        val vv = tsugiNoIchi(controller.houkou)
-        jiki.x += vv.toInt()
+
+    //加速度
+    var vJump = 0f
+    fun kasokudoJump(houkou:String):Float {
+        when (houkou) {
+            "jump" -> { return 1.0f }
+            "nashi" -> {return 0f }
+            else -> return 0f
+        }
+    }
+    //加速度で距離を求めて自機のｙに足す（引く）
+    fun jikiNoIchiTate(){
+
+        vJump = vJump + kasokudoJump(controller.houkou)
+        jiki.y += vJump.toInt()
     }
 
-
+    //んんん、まずはＪｕｍｐフレームはいるな
+    //押したときに計算が開始するような感じか
+    //そこで最初に＋30くらいする感じか
     fun tsugiNoSyori() {
         controller.clickPointCheck(clickX,clickY,clickState)
 
-        jikiNoIchi()    //課題用
+        //ジャンプ時の処理
+        if(controller.isJumpButton){
+
+        }
+
+        // こいつらは押されたボタンを無視していつづける
+        jikiNoIchiYoko()    //横方向　課題用
+        jikiNoIchiTate()    //縦方向  ジャンプ
 
         frame += 1  //繰り返し処理はここでやってる
         invalidate()
@@ -85,6 +108,28 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
 
 
 
+
+    override fun onDraw(canvas: Canvas) {
+        val bgPaint = Paint()
+        bgPaint.color = Color.argb(255, 0, 0, 255)   // 背景色
+        bgPaint.style = Paint.Style.FILL
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), bgPaint)
+        background.draw(canvas)
+        mapCreate(canvas)
+        jiki.draw(canvas)
+        controller.draw(canvas)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.kirerusan, BitmapFactory.Options())
+        canvas.drawBitmap(bitmap, jiki.x.toFloat()-40, jiki.y.toFloat()-45, null)
+    }
+
+    fun mapCreate(canvas:Canvas){
+        for (i in 0 until map.masu.size) {
+            for (j in 0 until map.masu[i].size) {
+                map.drawMap(canvas,i,j,map.masShurui(i,j),-worldOffsetX)
+            }
+        }
+    }
     fun lowcalCheck(houkou:String):Boolean {
         //とりあえずｘマスだけ //ｙはとりあえず１３固定
         val charamasu = worldOffsetCharacterX / map.MASU_SIZE   //キャラの世界位置
@@ -110,28 +155,6 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
         }
     }
 
-
-    override fun onDraw(canvas: Canvas) {
-        val bgPaint = Paint()
-        bgPaint.color = Color.argb(255, 0, 0, 255)   // 背景色
-        bgPaint.style = Paint.Style.FILL
-        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), bgPaint)
-        background.draw(canvas)
-        mapCreate(canvas)
-        jiki.draw(canvas)
-        controller.draw(canvas)
-
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.kirerusan, BitmapFactory.Options())
-        canvas.drawBitmap(bitmap, jiki.x.toFloat()-40, jiki.y.toFloat()-45, null)
-    }
-
-    fun mapCreate(canvas:Canvas){
-        for (i in 0 until map.masu.size) {
-            for (j in 0 until map.masu[i].size) {
-                map.drawMap(canvas,i,j,map.masShurui(i,j),-worldOffsetX)
-            }
-        }
-    }
 
     var clickNitenmeX = 0
     var clickNitenmeY = 0
