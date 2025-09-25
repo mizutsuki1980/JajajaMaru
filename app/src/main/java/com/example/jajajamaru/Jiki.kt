82740803cb0e636fd5293665892da8a99800b1bb
LAPTOP-7LL19PQQ\user <mizutuki1980@gmail.com>
2025-09-25 17:55:58 +0900

    ジャンプして障害物の上に着地する、はできた たぶん右方向にのみプラス１して計算しているので、左方向にうごくとダメ。 だがまー、それとないくはできた

--- app/src/main/java/com/example/jajajamaru/Jiki.kt
+++ app/src/main/java/com/example/jajajamaru/Jiki.kt
@@ -12,32 +12,33 @@ class Jiki(var x:Int, var y:Int) {
     var vYokoPlus = 0f
 
 
-    fun draw(canvas: Canvas){
+    fun draw(canvas: Canvas) {
         iro.style = Paint.Style.FILL
         iro.color = argb(255, 255, 255, 150)
-            //  canvas.drawCircle(x.toFloat(),(y).toFloat(),(ookisa/2).toFloat(),iro) //自機の描画
+        //  canvas.drawCircle(x.toFloat(),(y).toFloat(),(ookisa/2).toFloat(),iro) //自機の描画
     }
 
     var isJump = false
     var vJump = 0f
 
-    fun kasokudoJump():Float {
+    fun kasokudoJump(): Float {
         return -5.0f
     }
-    fun jumpSyori(controller: Controller){
-        if(controller.houkou=="jump"){
-            if(isJump==false){
-                isJump=true
+
+    fun jumpSyori(controller: Controller) {
+        if (controller.houkou == "jump") {
+            if (isJump == false) {
+                isJump = true
                 vJump = 50f
                 y -= vJump.toInt()
             }
         }
-        if(isJump) {
+        if (isJump) {
             vJump = vJump + kasokudoJump()
             y -= vJump.toInt()
         }
         //ジャンプの処理はこの辺で行っている。ｙが０地点ならジャンプを終了する、みたいなのを書く
-        if (y >= 500 && y <550){
+        if (y >= 500 && y < 550) {
             isJump = false
             vJump = 50f
             y = 500
@@ -48,39 +49,110 @@ class Jiki(var x:Int, var y:Int) {
     }
 
 
-    fun jikiYokoIdo(controller: Controller,map: Map) {
+    fun jikiYokoIdo(controller: Controller, map: Map) {
         val kasokudo = kasokudoYoko(controller.houkou)
         val vYokoPlusCheckyou = vYokoPlus + kasokudo
-        var syougaibutuCheck = syougaibutuHantei(vYokoPlusCheckyou,controller,map)
-        if(syougaibutuCheck) {
+        var syougaibutuCheck = false
+        var syougaibutuJump = false
+
+        //まず、ここでジャンプか、ジャンプじゃないか、分ける。
+        // ジャンプじゃないなら、今まで通りの処理でいい。
+
+        if (isJump) {
+            syougaibutuCheck = true
+            syougaibutuJump = syougaibutuJump(vYokoPlusCheckyou,controller, map) //この時点では判定だけする
+        } else {
+            syougaibutuCheck = syougaibutuHantei(vYokoPlusCheckyou, controller, map)
+        }
+
+        if (syougaibutuCheck) {
             vYokoPlus = vYokoPlus + kasokudo
             //速度制限
-            if(vYokoPlus>=30){ vYokoPlus = 30f} //１マス以上加速しないことで制限
-            if(vYokoPlus<=-30){ vYokoPlus = -30f} //１マス以上加速しないことで制限
+            if (vYokoPlus >= 30) {
+                vYokoPlus = 30f
+            } //１マス以上加速しないことで制限
+            if (vYokoPlus <= -30) {
+                vYokoPlus = -30f
+            } //１マス以上加速しないことで制限
             worldOffsetX += vYokoPlus.toInt()
             sekaix += vYokoPlus.toInt() //世界のｘだけ動いていれば、画面上のｘはどこでもいいのかもしれない
             migihidariCharaGamenIdoSeigen(controller)
-        }else{
-
+        } else {
             when (controller.houkou) {
                 "migi" -> {
                     vYokoPlus = 0f
-                    worldOffsetX +=  -17    //マスの半分をもどす
+                    worldOffsetX += -17    //マスの半分をもどす
                     sekaix += -17
                 }
+
                 "hidari" -> {
                     vYokoPlus = 0f
-                    worldOffsetX +=  17    //マスの半分をもどす
+                    worldOffsetX += 17    //マスの半分をもどす
                     sekaix += 17
                 }
-                "nashi" -> { vYokoPlus = 0f }
-                else -> vYokoPlus = 0f
 
+                "nashi" -> {
+                    vYokoPlus = 0f
+                }
+                else -> vYokoPlus = 0f
             }
-            //ぶつかった場合　右方向のみ
-        //なるほど、ぶつかった場合もsekaixを「ぶつかる直前」まで進めないとだめなのかな
         }
+
+        if (isJump) {
+            if(syougaibutuJump){
+             //もしジャンプかつ、障害物に当たってるよ、という判定なら、ここにくる
+
+            }else{
+                isJump = false
+            }
+        }
+
     }
+
+    fun syougaibutuJump( vYokoPlusCheckyou:Float,controller: Controller, map:Map):Boolean{
+        //x,yは既にわかっているわけで、そこから障害物かどうか判定する、
+
+        var checksekaix = sekaix + vYokoPlusCheckyou.toInt() //世界のｘだけ動いていれば、画面上のｘはどこでもいいのかもしれない
+        if (controller.houkou == "migi") { checksekaix += ookisa / 2 }
+        if (controller.houkou == "hidari") { checksekaix -= ookisa / 2 }
+        var checkBlock = checksekaix / 32 // 7マスから map.MASU_SIZE
+        val checkMasuSyuruiX = map.masu[13][checkBlock+1]  //listは０から!!!
+        // ここまでｘのはなし
+
+        var checksekaiy = y - vJump.toInt() //世界のｘだけ動いていれば、画面上のｘはどこでもいいのかもしれない
+        var yBlock = 0
+        if(checksekaiy<=500 && checksekaiy >=468){yBlock = 13}
+        if(checksekaiy<=467 && checksekaiy >=436){yBlock = 12}
+        if(checksekaiy<=435 && checksekaiy >=404){yBlock = 11}
+
+
+        val checkMasuSyuruiJump = map.masu[yBlock][checkBlock+1]
+
+
+
+        //ここでｙを含めた障害物をチェックする
+        //横移動との違いは、ｘ、ｙともにチェックする、ということだ。
+
+        //ここでチェックするマスの種類をチェックするが、ｙ方向にも必要となる
+        //１３が変わる？っていうこと？
+        //１３っていうのは５００～４６８っていうことだと思う。
+        //ｙが入っているから
+
+        //checkMasuSyuruiJumpにはMapの１，０が入っている、、、、はず
+
+        var checkKekka = false
+        when(checkMasuSyuruiJump){
+            0 -> { checkKekka = true }
+            1 -> {
+                //障害物にあたっている判定
+                checkKekka = false
+            }
+            else ->{checkKekka = true }
+        }
+        return checkKekka
+    }
+
+
 
     fun syougaibutuHantei(vYokoPlusCheckyou: Float, controller: Controller, map:Map):Boolean{
         var checksekaix = sekaix + vYokoPlusCheckyou.toInt() //世界のｘだけ動いていれば、画面上のｘはどこでもいいのかもしれない
@@ -106,7 +178,6 @@ class Jiki(var x:Int, var y:Int) {
         }
         return checkKekka
     }
-
 
 
     private fun migihidariCharaGamenIdoSeigen(controller: Controller) {
