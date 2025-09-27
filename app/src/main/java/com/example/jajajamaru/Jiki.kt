6fa1744f9fc7f99c2d997f34e31e9c0eb62eb11e
LAPTOP-7LL19PQQ\user <mizutuki1980@gmail.com>
2025-09-27 12:58:16 +0900

    うーん、なんだかわかんなくなってきた

--- app/src/main/java/com/example/jajajamaru/Jiki.kt
+++ app/src/main/java/com/example/jajajamaru/Jiki.kt
@@ -68,28 +68,21 @@ class Jiki(var x:Int, var y:Int) {
         val checkCharaMigihajiX = (checkX +(ookisa/2)).toInt()
         val checkCharaHidarihajiX = (checkX -(ookisa/2)).toInt()
 
-        //右端を決めるためのチェック
+        //右端でのチェック
         //次の右端の座標ｘはわかっているのだから、true か　falseで返せるはず。
-        if(mapCheck(map,checkCharaMigihajiX)){
-            jikiXido(controller)//障害物がなければ、はじめて時期を移動させる
-        }
-        //左端を決めるためのチェック
 
-
-        var syougaiCheckX = syougaiX(checkX, controller, map)//次の位置に障害物あるか？
-        if (isJump) { syougaiCheckX = false }//ジャンプなら障害物は無視
-
-        if (syougaiCheckX) { //横方向に障害物があった場合
-            when (controller.houkou) {
-                "migi" -> { syougaibutuSyoriX(-17,-17) }//右に障害物があれば半マス戻す
-                "hidari" -> { syougaibutuSyoriX(17,17) }//左に障害物があれば半マス戻す
-                "nashi" -> { xPlus = 0f }//なしだとどーすんの？ちゃんと決めてない。ここだ！多分すりぬけるの。
-                else -> xPlus = 0f
+        if (controller.houkou == "migi"){
+            if(mapCheck(map,checkCharaMigihajiX)){
+                worldOffsetX += xPlus.toInt()
+                sekaix += xPlus.toInt()
+            }else{
             }
-        } else { //障害物がなかった場合
-           // jikiXido(controller)//障害物がなければ、はじめて時期を移動させる
         }
+
+
     }
+
+
     fun mapCheck(map:Map,checkCharaMigihajiX:Int): Boolean{
         var check = true
         val checkBlock = checkCharaMigihajiX / 32
@@ -173,7 +166,7 @@ class Jiki(var x:Int, var y:Int) {
 
 
 
-    private fun CharaCameraIdoSeigen(controller: Controller) {
+    fun CharaCameraIdoSeigen(controller: Controller) {
         if (controller.houkou == "migi") {
             if (xPlus > 0) {
                 if (x <= 400) {
@@ -189,6 +182,8 @@ class Jiki(var x:Int, var y:Int) {
             }
         }
     }
+
+
     fun kasokudoYoko(houkou:String):Float {
         when (houkou) {
             "migi" -> {return 5.0f }
