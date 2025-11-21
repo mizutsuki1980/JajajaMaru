a8460141d2b0ffa2eabe7f4de2a1c6dd95121e7f
LAPTOP-7LL19PQQ\user <mizutuki1980@gmail.com>
2025-11-21 21:00:28 +0900

    shogaibutuJogeSayuu()のなかで修正をしていたが、 afterRakkaとafterSayuuに切り替えるだけで、 作業前、と、作業後、を交互に見れるのはいい作りだ
    Rakkaは速度が速くなる？のと、突き抜けてしまう問題が再発した

--- app/src/main/java/com/example/jajajamaru/Jiki.kt
+++ app/src/main/java/com/example/jajajamaru/Jiki.kt
@@ -99,7 +99,7 @@ class Jiki(val initialPos: Vec2D) {
             afterSayuu
         }
 
-        return afterRakka
+        return afterSayuu
     }
 
     fun mapCheckYRakka(map:Map,x1cand:Int,y1Cand:Int):Boolean{
