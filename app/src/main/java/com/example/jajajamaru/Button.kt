14e306009f093895480c08ac96cbf787dc75ed5b
LAPTOP-7LL19PQQ\user <mizutuki1980@gmail.com>
2025-12-18 17:26:02 +0900

    とりあえず保存

--- app/src/main/java/com/example/jajajamaru/Button.kt
+++ app/src/main/java/com/example/jajajamaru/Button.kt
@@ -3,4 +3,5 @@ package com.example.jajajamaru
 import android.graphics.Paint
 import android.graphics.Rect
 
-data class Button(val x:Int, val y:Int, val xOokisa:Int, val yOokisa:Int, var paint:Paint,var rect:Rect)
+data class
+Button(val x:Int, val y:Int, val xOokisa:Int, val yOokisa:Int, var paint:Paint,var rect:Rect)
