4c33949913cce5edb21d9607fcd6b0cdb02c0488
mizutsuki1980 <mizutuki1980@gmail.com>
2026-03-02 10:38:02 +0900

    敵の画像を変更

--- app/src/main/java/com/example/jajajamaru/MyCustomView.kt
+++ app/src/main/java/com/example/jajajamaru/MyCustomView.kt
@@ -73,7 +73,7 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
         val bitmap = BitmapFactory.decodeResource(resources, R.drawable.kirerusanpng, BitmapFactory.Options())
         canvas.drawBitmap(bitmap, jiki.initialPos.x.toFloat()-40, jiki.sekaipos.y.toFloat()-75, null)
         teki.draw(canvas,jiki)
-        val bitmapTeki = BitmapFactory.decodeResource(resources, R.drawable.mtoukarokugo, BitmapFactory.Options())
+        val bitmapTeki = BitmapFactory.decodeResource(resources, R.drawable.teki, BitmapFactory.Options())
         canvas.drawBitmap(bitmapTeki, teki.xx.toFloat()-40, teki.sekaipos.y.toFloat()-75, null)
 
 
